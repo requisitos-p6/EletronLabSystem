d8787e4ee42b6db75d967ef3a863cac5bfe697b0
Renata Soares <renata.sds09@gmail.com>
2016-06-11 17:15:58 -0300

    Validating cpf

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
+
+	require "cpf_cnpj"
+
+	VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length:{maximum: 60}
+  validates :nickname, presence: true, length:{maximum: 40}, uniqueness: true
+  validates :email, presence: true, length:{maximum: 255},
+  format: { with:  VALID_EMAIL },
+  uniqueness: { case_sensitive: false }
+  validates :address, presence:true
+	validate :validate_cpf
+
+	def validate_cpf
+		unless CPF.valid?(self.cpf)
+			errors.add(:cpf, "inválido")
+		end
+	end
+
 end
