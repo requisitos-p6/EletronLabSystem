04d7def1230c12e050de85357b77b98c1fd20f9d
João Vitor Araujo <joao18araujo@gmail.com>
2016-06-14 11:39:39 -0300

    Granting access only to admin in profile editing
    Signed-off-by: João Vitor Araujo <joao18araujo@gmail.com>
    Signed-off-by: Jéssica Cristina <jessica.cris1127@gmail.com>

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
+  before_action :verify_admin_permission
 
   # GET /profiles
   # GET /profiles.json
