e1d22ba7a9330cc60cce9159dfc63926dd8cf52f
ArthurTemporim <arthurrtl@gmail.com>
2016-06-28 15:39:46 -0300

    Translate some views.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :profiles
 
   get 'signup' => 'users#new'
+  get 'scales' => 'scales#show'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   post 'logout' => 'sessions#destroy'
